d6ae5341a18eef8753194f884ab7d35bb9c130e5
Alexander Morales <alexandermorales.n1@gmail.com>
2025-06-12 18:12:12 +0000

    fix navbar positioning

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -6,13 +6,13 @@ export const Navbar = () => {
 	return (
 		<div className="nav-bar">
 			<nav className="navbar navbar-expand-lg bg-body-tertiary">
-				<div>
+				<div className="col-4">
 					<a className="navbar-brand" href="#">Log in</a>
 				</div>
-				<div>
-					<img src={goatImg} alt="logo" style={{width:'50px'}}/>
+				<div className="col-4 text-center">
+					<img src={goatImg} alt="logo" style={{ width: '50px' }} />
 				</div>
-				<div>
+				<div className="d-flex col-4 justify-content-end">
 					<input type="text" placeholder="user" style={{ width: '100px', border: 'black solid 1px' }} className="mx-1 rounded-pill px-2" />
 					<input type="password" placeholder="pin" style={{ width: '100px', border: 'black solid 1px' }} className="mx-1 rounded-pill px-2" />
 					<button style={{ width: '50px', border: 'black solid 1px' }} className="mx-1 rounded-pill px-2">go</button>
